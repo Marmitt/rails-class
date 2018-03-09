4b32ab7839d3db0380f5264072a2996172a41461
Marmitt <lmateus.marmitt@gmail.com>
2018-03-08 19:40:09 -0500

    Segundo commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   def index
     @product = Product.last
+    
+    codigo_produto = params['numero']
+    quantidade = params['quantidade'] || 0
+    
+    prod = Product.find_by_id(codigo_produto || 0)
+    @produto_encontrado = prod && prod.name || 'Produto não encontrado'
+    
+    @valor_total = prod ? quantidade.to_f * prod.price  : 0
   end
   
   def about
